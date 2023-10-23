5ac809d1b5bbc0886e5d09f33764a611ab747885
Miras <miamib531@gmail.com>
2023-10-23 19:20:11 +0600

    feat: create necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchBarComponent } from './search/search-bar/search-bar.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        SearchResultsComponent,
+        SearchItemComponent,
+        SearchBarComponent
     ],
     imports: [
         BrowserModule,
